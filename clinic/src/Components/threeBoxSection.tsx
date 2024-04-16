7c4b1f196bf69e0add47dc1623b009b27a5ebbac
Aver <kuba.grych@gmail.com>
2024-04-16 17:05:24 +0200

    xd!

---
+++ clinic/src/Components/threeBoxSection.tsx
@@ -0,0 +1,12 @@
+import './ComponentStyles/threeBoxSection.css'
+function threeBoxSection() {
+    return (
+        <div className={'three-block-container'}>
+            <div className={'single-block first'}></div>
+            <div className={'single-block second'}></div>
+            <div className={'single-block third'}></div>
+        </div>
+    )
+}
+
+export default threeBoxSection
