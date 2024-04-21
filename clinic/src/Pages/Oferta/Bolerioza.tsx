610297599ebb08eb74747a8256454fba5996f517
ayanover <kuba.grych@gmail.com>
2024-04-21 17:59:07 +0200

    xd

--- clinic/src/Pages/Oferta/Bolerioza.tsx
+++ clinic/src/Pages/Oferta/Bolerioza.tsx
@@ -1,10 +1,58 @@
-import '../PageStyles/Blog.css'
-import InProgress from "../inProgress.tsx";
-
+import '../PageStyles/Biorezonans.css'
+import photo from '../../assets/gabinet.jpg'
+import bioresonance from '../../assets/bolerioza.jpg'
 
 function Bolerioza() {
     return(
-        <InProgress/>
+        <div className={'home-container-bio'}>
+            <div className={'photo-container-bio'}>
+                <img src={photo} alt={'reception-desk'}/>
+            </div>
+            <div className={'content-container-bio'}>
+                <div className={'section-container'}>
+                    <h2>Terapia <span>Boleriozy</span></h2>
+
+                    <div className={'content-section photo-section'}>
+                        <img src={bioresonance} alt={'biores'}/>
+                        <a><span>Borelioza </span>  to bardzo poważne zagrożenie. Niewłaściwie zdiagnozowana i leczona może
+                            powodować dotkliwe konsekwencje zdrowotne dotyczące m.in. tkanki łącznej, mięśniowej i
+                            nerwowej. Testy wykonywane z krwi np. Elisa czy Western Blot mają skutecznośc diagnostyczną
+                            w przedzialle tylko ok 40%.
+                            <br/>
+                            W naszym gabinecie proponujemy skuteczne diagnozowanie i
+                            terapię boreliozy biorezonansem BICOM Optima oraz doradztwo w terapii ziołowej.
+                            <br/>
+                            Zapraszamy
+                            do zapoznania się z naszą ofertą leczenia alternatywnego.
+                        </a>
+
+                    </div>
+                    <div className={'content-section'}>
+                        <a>
+                            <br/>
+                            <span>Diagnostyka i leczenie boreliozy biorezonansem</span>
+                            <br/>
+                            W gabinetach BioSfera w Rzeszowie oferujemy terapię boreliozy za pomocą biorezonansu oraz
+                            indywidualnie dobranych protokołow ziołowych.
+                            <br/>
+                            Posiadamy obszerny i nowoczesny test, dzięki
+                            któremu jesteśmy w stanie zdiagnozować występowanie aż 61 patogenów powiązanych z
+                            chorobami odkleszczowymi. Na tej podstawie przygotowujemy skuteczneą terapię. Terapia
+                            leczenia boreliozy biorezonansem BICOM Optima polega na analizie wzorów częstotliwości i
+                            występowania patogenów, a następnie na wysyłaniu do organizmu odpowiedzi zwrotnej. Jej
+                            celem jest zredukowanie, a z czasem wyeliminowanie problemu zdrowotnego.
+                            <br/>
+                            Biorezonans stanowi również cenne wsparcie dla klasycznego leczenia boreliozy z
+                            wykorzystaniem środków farmakologicznych. Bakterie ulegają zniszczeniu, a organizm jest
+                            stymulowany do jeszcze skuteczniejszej walki z zagrożeniem. Dzięki temu biorezonans pozwala
+                            na poradzenie sobie z formami borelii, które są oporne na działanie antybiotyków. Czas trwania
+                            terapii zależy od stopnia rozsiania infekcji, typu bakterii oraz obecnego stanu organizmu
+                            pacjenta.
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- clinic/src/Pages/Oferta/Pasozyty.tsx
+++ clinic/src/Pages/Oferta/Pasozyty.tsx
@@ -1,10 +1,56 @@
-import '../PageStyles/Blog.css'
-import InProgress from "../inProgress.tsx";
-
+import '../PageStyles/Biorezonans.css'
+import photo from '../../assets/gabinet.jpg'
+import bioresonance from '../../assets/grzyby.jpg'
 
 function Pasozyty() {
     return(
-        <InProgress/>
+        <div className={'home-container-bio'}>
+            <div className={'photo-container-bio'}>
+                <img src={photo} alt={'reception-desk'}/>
+            </div>
+            <div className={'content-container-bio'}>
+                <div className={'section-container'}>
+                    <h2>Terapia <span>Grzybów</span></h2>
+
+                    <div className={'content-section photo-section'}>
+                        <img src={bioresonance} alt={'biores'}/>
+                        <a><span>Biorezonans</span> jest specjalistyczną metodą diagnostyki i terapii, która
+                            zapoczątkowała zupełnie
+                            nowe podejście do medycyny. Zasady obowiązujące w tej metodzie potwierdzają najnowsze
+                            wyniki badań w dziedzinie biofizyki i mechaniki kwantowej, dotychczasowo pomijane w
+                            medycynie konwencjonalnej.
+                            <br/>
+                            <br/>
+                            W terapii biorezonansowej wykorzystuje się istniejące w organizmie człowieka drgania
+                            elektromagnetyczne, które są nadrzędne w stosunku do procesów biochemicznych i nimi
+                            sterują.
+                        </a>
+
+                    </div>
+                    <div className={'content-section'}>
+                        <a>
+                            <br/>
+                            <br/>
+                            <span>Biorezonans</span> potrafi wykryć nawet ukryte przyczyny choroby i przeprowadzić delikatną formę
+                            kuracji leczniczej.
+                            Opierając się na znajomości dokładnie określonych wartości liczbowych pól
+                            elektromagnetycznych możliwe jest ustalenie, czy narząd emituje pole odpowiadające
+                            zdrowemu organowi, czy uległ on procesowi patologicznemu. Co więcej, znane są częstotliwości
+                            emitowane przez każdą poszczególną bakterię, pasożyt, wirus. Warto podkreślić, że terapia
+                            biorezonansem nie powoduje skutków ubocznych, więc jest całkowicie bezpieczna
+                            <br/>
+                            <br/>
+                            Biorezonansowe testowanie alergenów jest w pełni uznawane przez Światową Organizację
+                            Zdrowia WHO (Od 2019 r.) – jako medycyna naturalna oraz komplementarna, a wyniki są na
+                            Zachodzie Europy honorowane na równi z testami skórnym, płatkowym lub z krwi.
+                            <br/>
+                            <br/>
+                            Zakres testów BRT jest jednak znacznie szerszy, gdyż sprawdzeniu możemy poddać praktycznie
+                            wszystko co spożywamy, używamy lub nas otacza.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
